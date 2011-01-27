8c5908544a6a0809a28a24664fb6e8361bb90a46
Till Mossakowski <till@communtu.de>
2011-01-27 20:13:45 +0100

    dojo chapter 5

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
   describe "GET /layout_links" do
-    it "works! (now write some real specs)" do
-      get layout_links_path
-    end
+
   end
+  
+    it "should have a signup page at '/signup'" do
+      get '/signup'
+      response.should have_selector("title", :content => "Sign up") 
+    end  
+
 end
